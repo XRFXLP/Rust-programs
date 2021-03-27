3a383e6a24845a49a61d0007ab064c69f7b37200
Ajay Mishra <Ajaym6405@gmail.com>
2021-03-27 17:04:13 +0530

    Create count_star.rs

---
+++ count_star.rs
@@ -0,0 +1,16 @@
+// This one looks really cool :P
+
+fn countStars(adj: Vec<Vec<bool>>) -> i32{
+    let graph : Vec<Vec<u32>> = adj.iter()
+                                   .map(|x| x.iter()
+                                             .enumerate()
+                                             .filter(|(i, j)| **j == true)
+                                             .map(|(i, j)| i as u32)
+                                             .collect())
+                                   .collect();
+    graph.iter().enumerate()
+                .filter(|(i, j)| j.len() > 0 && j.iter().filter(|&&x| graph[x as usize].len() == 1).count() == j.len())
+                .map(|(i, x)| if x.len() == 1 { 0.5_f32 } else {1 as f32})
+                .sum::<f32>() as i32
+}
+                  
