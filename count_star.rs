34cfa7135a13916f3458a6ecd5409441aee16c39
Ajay Mishra <Ajaym6405@gmail.com>
2021-03-28 12:31:06 +0530

    Update count_star.rs

--- count_star.rs
+++ count_star.rs
@@ -5,13 +5,13 @@ fn countStars(adj: Vec<Vec<bool>>) -> i32{
                                              .filter(|(i, j)| **j == true)
                                              .map(|(i, j)| i as u32)
                                              .collect())
-                                   .collect();
-    graph.iter()
-         .enumerate()
-         .filter(|(i, j)| j.len() > 0 && j.iter()
-                                          .filter(|&&x| graph[x as usize].len() == 1)
-                                          .count() == j.len())
-         .map(|(i, x)| if x.len() == 1 { 0.5_f32 } else {1 as f32})
-         .sum::<f32>() as i32
+                                   .collect()
+                                   .iter()
+                                   .enumerate()
+                                   .filter(|(i, j)| j.len() > 0 && j.iter()
+                                                                  .filter(|&&x| graph[x as usize].len() == 1)
+                                                                  .count() == j.len())
+                                   .map(|(i, x)| if x.len() == 1 { 0.5_f32 } else {1 as f32})
+                                   .sum::<f32>() as i32
 }
                   
